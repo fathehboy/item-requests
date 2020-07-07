04393ff625dce5a58a60240cf2d62109f05d2f1f
fathehboy <fatheh.alif@gmail.com>
2020-07-07 21:48:28 +0700

    change db

--- fungsi/koneksi.php
+++ fungsi/koneksi.php
@@ -2,7 +2,7 @@
 
 $host = "localhost";
 $username = "root";
-$password = "";
+$password = "babi8481";
 $database = "material-teknik";
 
 $koneksi = mysqli_connect($host, $username, $password, $database);
