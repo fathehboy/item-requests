c488b93c5fc349180887c5a34f29aea628cf21a3
fathehboy <fatheh.alif@gmail.com>
2020-07-08 15:33:57 +0700

    role authentication

--- fungsi/ceklogin.php
+++ fungsi/ceklogin.php
@@ -1,15 +1,10 @@
-<?php  
+<?php
 
+function levelcheck()
+{
 	if (isset($_SESSION['login'])) {
-		if ($_SESSION['level'] == "unit_pelayanan") {
-			header("location:unit_pelayanan/index.php");
-		} else if ($_SESSION['level'] == "admin_gudang"){
-			header("location:admin_gudang/index.php");
-		} else if ($_SESSION['level'] == "asisten_manajer"){
-			header("location:asmen_gudang/index.php");
-		} else {
-			header("location:index.php");
-		}
+		header('location:' . $_SESSION['level'] . '/index.php');
+	} else {
+		header("location:index.php");
 	}
-
-?>
+}
