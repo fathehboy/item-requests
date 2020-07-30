07f35fe26e6b0df2056a222247fc9d8c05371c10
andrebagoes <andrebagussaputra@gmail.com>
2020-07-30 21:49:22 +0800

    mamtap

--- pen_sdm&umum/cek_asmen.php
+++ pen_sdm&umum/cek_asmen.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if ($_SESSION['login'] == 1 && $_SESSION['level'] != 'asmen_gudang') {
+if ($_SESSION['login'] == 1 && $_SESSION['level'] != 'pen_sdm&umum') {
     header('Location:../' . $_SESSION['level']);
 } else if ($_SESSION['login'] != 1) {
     echo 'Please login first';
