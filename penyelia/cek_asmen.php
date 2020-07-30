cb3c4a58a8573a549a305581fe0bb0ac05ac2253
andrebagoes <andrebagussaputra@gmail.com>
2020-07-30 23:14:17 +0800

    Mantap4

--- penyelia/cek_asmen.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if ($_SESSION['login'] == 1 && $_SESSION['level'] != 'penyelia') {
-    header('Location:../' . $_SESSION['level']);
-} else if ($_SESSION['login'] != 1) {
-    echo 'Please login first';
-    header('Location:../index.php');
-}
-?>
