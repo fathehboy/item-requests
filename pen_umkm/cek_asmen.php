4f7ef98cf003f380f21de740a33c97150976cd96
andrebagoes <andrebagussaputra@gmail.com>
2020-07-30 21:31:05 +0800

    ok DOne

---
+++ pen_umkm/cek_asmen.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+if ($_SESSION['login'] == 1 && $_SESSION['level'] != 'pen_umkm') {
+    header('Location:../' . $_SESSION['level']);
+} else if ($_SESSION['login'] != 1) {
+    echo 'Please login first';
+    header('Location:../index.php');
+}
+?>
---
+++ pen_umkm/cek_unit.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+if ($_SESSION['login'] == 1 && $_SESSION['level'] != 'asmen_gudang') {
+    header('Location:../' . $_SESSION['level']);
+} else if ($_SESSION['login'] != 1) {
+    echo 'Please login first';
+    header('Location:../index.php');
+}
+
+?>
